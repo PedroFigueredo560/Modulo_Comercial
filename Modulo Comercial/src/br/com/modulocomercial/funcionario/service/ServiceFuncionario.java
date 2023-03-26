eaa499cd988cecfbcb8763bb013047fae69581e0
PedroFigueredo560 <114409376+PedroFigueredo560@users.noreply.github.com>
2023-03-26 20:32:01 -0300

    Add files via upload
    Adicionando pacotes, e arquivos a serem modificados

---
+++ Modulo Comercial/src/br/com/modulocomercial/funcionario/service/ServiceFuncionario.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.modulocomercial.funcionario.service;
+
+/**
+ *
+ * @author ph715
+ */
+public class ServiceFuncionario {
+    
+}
