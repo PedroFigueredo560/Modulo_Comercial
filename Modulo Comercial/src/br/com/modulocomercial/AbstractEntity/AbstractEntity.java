2fab61203819a1ee988509feee8872d41f29dc6e
[Pedro Figueredo] <ph71542@gmail.com>
2023-03-29 15:06:07 -0300

    Colocando persistence e alterando algumas coisas

--- Modulo Comercial/src/br/com/modulocomercial/AbstractEntity/AbstractEntity.java
+++ Modulo Comercial/src/br/com/modulocomercial/AbstractEntity/AbstractEntity.java
@@ -3,26 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
-
-/**
- *
- * @author ph715
-*/
-
-@MappedSuperclass
-public class AbstractEntity {
-    //ID das entidades
-    private long id;
-    @Id 
-    @GeneratedValue
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-}
+ import javax.persistence.GeneratedValue;
+ import javax.persistence.Id;
+ import javax.persistence.MappedSuperclass;
+ 
+ /**
+  *
+  * @author ph715
+ */
+ 
+ @MappedSuperclass
+ public class AbstractEntity {
+     //ID das entidades
+     private long id;
+     @Id 
+     @GeneratedValue
+     public long getId() {
+         return id;
+     }
+ 
+     public void setId(long id) {
+         this.id = id;
+     }
+ }
+ 
