612bbe676b81aa69a385827ef662103768430d7e
[Pedro Figueredo] <ph71542@gmail.com>
2023-03-31 11:10:26 -0300

    Arrumando o local das views e imagens no projeto

--- Modulo Comercial/src/br/com/modulocomercial/view/Register_ScreenUser.java
+++ Modulo Comercial/src/br/com/modulocomercial/view/Register_ScreenUser.java
@@ -1,3 +1,4 @@
+package br.com.modulocomercial.view;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
