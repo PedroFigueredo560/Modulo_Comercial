e7acc3b8b94cfa2aae4fc2de9d313aae03a6add1
[Pedro Figueredo] <ph71542@gmail.com>
2023-04-02 12:43:31 -0300

    Arrumando problemas com o persistence, adicionando jars

--- Modulo Comercial/src/br/com/modulocomercial/AbstractEntity/AbstractEntity.java
+++ Modulo Comercial/src/br/com/modulocomercial/AbstractEntity/AbstractEntity.java
@@ -1,3 +1,5 @@
+package br.com.modulocomercial.AbstractEntity;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
